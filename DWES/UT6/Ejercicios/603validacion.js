5c035a5b3432c18d804c833d55ca47f8b9622a04
LexLoneWolf <lareone91@gmail.com>
2021-01-12 15:17:14 +0100

    Actualizo 603validacion

--- DWES/UT6/Ejercicios/603validacion.js
+++ DWES/UT6/Ejercicios/603validacion.js
@@ -8,46 +8,21 @@ function validarFormulario() {
         alert("El campo Nombre es obligatorio");
         formulario.nombre.focus();
         document.getElementById("nombre").style.borderColor = "red";
-        return;
-    } else {
-        let cadena = /^[a-zA-Z ]*$/;
-        if (!formulario.nombre.value.match(cadena)) {
-            alert("Formato de nombre incorrecto");
-            formulario.nombre.focus();
-            document.getElementById("nombre").style.borderColor = "red";
-            return;
-        }
+        return "";
     }
 
     if (formulario.apellidos.value == "") {
         alert("El campo Apellidos es obligatorio");
         formulario.apellidos.focus();
         document.getElementById("apellidos").style.borderColor = "red";
-        return;
-    } else {
-        let cadena = /^[a-zA-Z ]*$/;
-        if (!formulario.apellidos.value.match(cadena)) {
-            alert("Formato de apellidos incorrecto");
-            formulario.apellidos.focus();
-            document.getElementById("apellidos").style.borderColor = "red";
-            return;
-        }
+        return "";
     }
 
     if (formulario.email.value == "") {
         alert("El campo email es obligatorio");
         formulario.email.focus();
         document.getElementById("email").style.borderColor = "red";
-        return;
-    } else {
-        let patronEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-        if (!formulario.email.value.match(patronEmail)) {
-            alert("formato de email incorrecto");
-            formulario.email.focus();
-            document.getElementById("email").style.borderColor = "red";
-            return;
-        }
+        return "";
     }
 
     if (formulario.url.value == "") {
@@ -55,23 +30,13 @@ function validarFormulario() {
         formulario.url.focus();
         document.getElementById("url").style.borderColor = "red";
         return "";
-    } else {
-        let expRegUrl = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-        if (!formulario.url.value.match(expRegUrl)) {
-            alert("Formato de URL incorrecto");
-            formulario.url.focus();
-            document.getElementById("url").style.borderColor = "red";
-            return;
-        }
     }
 
-    if (formulario.convivientes.value < 0 || formulario.convivientes.value > 10) {
-        alert("El número de convivientes debe ser estre 0 y 10");
+    if (formulario.convivientes.value == 0) {
+        alert("El número de convivientes debe ser entre 1 y 10");
         formulario.convivientes.focus();
         document.getElementById("convivientes").style.borderColor = "red";
         formulario.convivientes.value = 0;
         return;
     }
-
-    formulario.submit();
 }
--- DWES/UT6/Ejercicios/603validacion.php
+++ DWES/UT6/Ejercicios/603validacion.php
@@ -1,45 +1,131 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>.error {color: #FF0000}</style>
+    <title>603validacion</title>
+</head>
+<body>
+    
 <?php 
 
-    if (!empty($_POST["nombre"])) { ?>
+    $nombre = $_POST["nombre"] ?? "";
+    $apellidos = $_POST["apellidos"] ?? "";
+    $email = $_POST["email"] ?? "";
+    $url = $_POST["url"] ?? "";
+    $genero = $_POST["genero"] ?? "";
+    $convivientes = $_POST["convivientes"] ?? 0;
+    $aficiones = $_POST["aficiones"] ?? [];
+    $menu = $_POST["menu"] ?? [];
+    $obligatorio = "Campo obligatorio";
+    $nombreErr = $apellidosErr = $emailErr = $urlErr = $generoErr = $convivientesErr = "";
+    $ok = false;
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $ok = true;
+        if (empty($_POST["nombre"])) {
+            $nombreErr = $obligatorio;
+            $ok = false;
+        } else {
+            $opciones = array("options" => array("regexp" => "/^[a-zA-Záéíóú\s]*$/"));
+            if (!filter_var($nombre, FILTER_VALIDATE_REGEXP, $opciones)) {
+                $nombreErr = "Sólo se permiten letras y espacios en blanco";
+                $ok = false;
+            }
+        }
+
+        if (empty($_POST["apellidos"])) {
+            $apellidosErr = $obligatorio;
+            $ok = false;
+        } else {
+            $opciones = array("options" => array("regexp" => "/^[a-zA-Záéíóú\s]*$/"));
+            if (!filter_var($apellidos, FILTER_VALIDATE_REGEXP, $opciones)) {
+                $apellidosErr = "Sólo se permiten letras y espacios en blanco";
+                $ok = false;
+            }
+        }
+
+        if (empty($_POST["email"])) {
+            $emailErr = $obligatorio;
+            $ok = false;
+        } else {
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Formato de email incorrecto";
+                $ok = false;
+            }
+        }
+
+        if (empty($_POST["url"])) {
+            $urlErr = $obligatorio;
+            $ok = false;
+        } else {
+            if (!filter_var($url, FILTER_VALIDATE_URL)) {
+                $urlErr = "Formato de URL incorrecto";
+                $ok = false;
+            }
+        }
+
+        if (empty($_POST["genero"])) {
+            $generoErr = $obligatorio;
+            $ok = false;
+        } 
+
+        if (empty($_POST["convivientes"])) {
+            $convivientesErr = $obligatorio;
+            $ok = false;
+        } else {
+            $opciones = array("options" => array("min_range" => 1, "max_range" => 10));
+            if (!filter_var($convivientes, FILTER_VALIDATE_INT, $opciones)) {
+                $convivientesErr = "Valores entre 1 y 10";
+                $ok = false;
+            }
+        }
+    }
+
+    if ($ok) { ?>
         
         <table border="10">
             <tr>
                 <th>Nombre</th>
-                <td><?php echo $_POST["nombre"] . " " . $_POST["apellidos"] ?></td>
+                <td><?php echo $nombre . " " . $apellidos; ?></td>
             </tr>
             <tr>
                 <th>Email</th>
-                <td><?= $_POST["email"] ?></td>
+                <td><?= $email ?></td>
             </tr>
             <tr>
                 <th>Página personal</th>
-                <td><?= $_POST["url"] ?></td>
+                <td><?= $url ?></td>
+            </tr>
+            <tr>
+                <th>Género</th>
+                <td><?= $genero ?></td>
             </tr>
             <tr>
                 <th>Número de convivientes</th>
-                <td><?= $_POST["convivientes"] ?></td>
+                <td><?= $convivientes ?></td>
             </tr>
-            <?php if(isset($_POST["aficiones"])) { ?>
+            <?php if(isset($aficiones)) { ?>
             <tr>
                 <th>Aficiones</th>
                 <td>
                     <ul>
                         <?php
-                        foreach($_POST["aficiones"] as $aficion) { ?>
+                        foreach($aficiones as $aficion) { ?>
                             <li><?= $aficion ?></li>
                         <?php  } ?>
                     </ul>
                 </td>
             </tr>
             <?php }
-            if (isset($_POST["menu"])) { ?>
+            if (isset($menu)) { ?>
             <tr>
                 <th>Platos favoritos</th>
                 <td>
                     <ul>
                         <?php 
-                        foreach ($_POST["menu"] as $plato) { ?>
+                        foreach ($menu as $plato) { ?>
                             <li><?= $plato ?></li>
                         <?php } ?>
                     </ul>
@@ -49,36 +135,49 @@
         </table>
     <?php  } else { ?>
         <script src="603validacion.js"></script>
-        <form id="formulario" action="603validacion.php" method="POST">
+        <form id="formulario" action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" onsubmit="return validarFormulario()">
             <label for="nombre">Nombre: </label>
-            <input type="text" id="nombre" name="nombre" /><br />
+            <input type="text" id="nombre" name="nombre" value="<?= $nombre ?>" required/>
+            <span class="error">* <?= $nombreErr ?></span><br />
 
             <label for="apellidos">Apellidos: </label>
-            <input type="text" id="apellidos" name="apellidos" required /><br />
+            <input type="text" id="apellidos" name="apellidos" value="<?= $apellidos ?>" required />
+            <span class="error">* <?= $apellidosErr ?></span><br />
 
             <label for="email">Email: </label>
-            <input type="email" name="email" id="email" required /><br />
+            <input type="email" name="email" id="email" value="<?= $email ?>" required />
+            <span class="error">* <?= $emailErr ?></span><br />
 
             <label for="url">URL página personal</label>
-            <input type="url" name="url" id="url" required /><br />
+            <input type="url" name="url" id="url" value="<?= $url ?>" required />
+            <span class="error">* <?= $urlErr ?></span><br />
+
+            <input type="radio" name="genero" value="hombre" <?php if(isset($genero) && $genero == "hombre") echo "checked"; ?>/> Hombre
+            <input type="radio" name="genero" id="mujer" value="mujer" <?php if(isset($genero) && $genero == "mujer") echo "checked"; ?>/> Mujer
+            <input type="radio" name="genero" id="otro" value="otro" <?php if(isset($genero) && $genero == "otro") echo "checked"; ?>/> Otro
+            <span class="error">*  <?= $generoErr ?></span><br />
 
             <label for="convivientes">Numero de convivientes en el domicilio</label>
-            <input type="number" id="convivientes" name="convivientes" required /><br />
+            <input type="number" id="convivientes" name="convivientes" value="<?= $convivientes ?>" required />
+            <span class="error">* <?= $convivientesErr ?></span><br />
             
             <label for="aficiones[]">Aficiones</label><br />
-            <input type="checkbox" name="aficiones[]" value="videojuegos" /> Videojuegos<br />
-            <input type="checkbox" name="aficiones[]" value="guitarra" /> Guitarra<br />
-            <input type="checkbox" name="aficiones[]" value="cine" /> Cine<br />
-            <input type="checkbox" name="aficiones[]" value="programar"/> Programar<br />
+            <input type="checkbox" name="aficiones[]" value="videojuegos" <?php if (in_array("videojuegos", $aficiones)) echo "checked"; ?> /> Videojuegos<br />
+            <input type="checkbox" name="aficiones[]" value="guitarra" <?php if (in_array("guitarra", $aficiones)) echo "checked"; ?> /> Guitarra<br />
+            <input type="checkbox" name="aficiones[]" value="cine" <?php if (in_array("cine", $aficiones)) echo "checked"; ?> /> Cine<br />
+            <input type="checkbox" name="aficiones[]" value="programar" <?php if (in_array("programar", $aficiones)) echo "checked"; ?> /> Programar<br />
 
             <label for="menu[]">Platos favoritos</label><br />
             <select name="menu[]" id="menu" multiple>
-            <option value="carbonara">Carbonara</option>
-            <option value="croquetas">Croquetas</option>
-            <option value="pizza">Pizza</option>
-            <option value="hamburguesa">Hamburguesa</option>
+            <option value="carbonara" <?php if (in_array("carbonara", $menu)) echo "selected"; ?>>Carbonara</option>
+            <option value="croquetas" <?php if (in_array("croquetas", $menu)) echo "selected"; ?>>Croquetas</option>
+            <option value="pizza" <?php if (in_array("pizza", $menu)) echo "selected"; ?>>Pizza</option>
+            <option value="hamburguesa" <?php if (in_array("hamburguesa", $menu)) echo "selected"; ?>>Hamburguesa</option>
             </select><br/>
 
-            <input type="button" value="enviar" onclick="validarFormulario()" />
+            <input type="submit" value="enviar" />
         </form>
     <?php } ?>
+
+</body>
+</html>
