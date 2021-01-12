da1edae68911a5cb943d05de515574b9f2ad1ceb
LexLoneWolf <lareone91@gmail.com>
2021-01-12 22:45:35 +0100

    604subida.php y 605subidaImagen.php

---
+++ DWES/UT6/Ejercicios/604subida.php
@@ -0,0 +1,66 @@
+<?php 
+
+$anchura = $_POST["anchura"] ?? 0;
+$altura = $_POST["altura"] ?? 0;
+$alturaErr = $anchuraErr = $archivoErr = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+    if (empty($anchura)) {
+        $anchuraErr = "Campo obligatorio";
+       
+    }
+
+    if (empty($altura)) {
+        $alturaErr = "Campo obligatorio";
+        
+    }
+
+    if (!empty($altura) && !empty($anchura)) {
+        if (is_uploaded_file($_FILES['archivoEnviado']['tmp_name'])) {
+            if ($_FILES['archivoEnviado']['size'] < 1024) {
+                $nombre = $_FILES['archivoEnviado']['name'];
+                move_uploaded_file($_FILES['archivoEnviado']['tmp_name'], "./uploads/{$nombre}");
+            } else {
+                $archivoErr = "Archivo demasiado grande. Tamaño máximo: 1024 bytes";
+            } 
+        } else {
+            $archivoErr = "No se ha subido ningún archivo";
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es-ES">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>.error {color: #FF0000}</style>
+    <title>605subidaImagen</title>
+</head>
+<body>
+
+    <form enctype="multipart/form-data" action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+        <label for="anchura">Anchura:</label>
+        <input type="number" name="anchura" value="<?= $anchura ?>" required />
+        <span class="error">* <?= $anchuraErr ?></span><br />
+
+        <label for="altura">Altura:</label>
+        <input type="number" name="altura" value="<?= $altura ?>" required />
+        <span class="error">* <?= $alturaErr ?></span><br />
+
+        Archivo: <input type="file" name="archivoEnviado" required />
+        <span class="error">* <?= $archivoErr ?></span><br />
+        <input type="submit" name="btnSubir" value="Subir" />
+        
+    </form>
+
+    <?php if (isset($nombre)) { ?>
+        <p>Archivo: <?= $nombre ?> subido con éxito</p>
+        <p>Tamaño: <?= $_FILES['archivoEnviado']['size']?></p>
+        <p>Tipo: <?= $_FILES['archivoEnviado']['type'] ?></p>
+    <?php } ?>
+</body>
+</html>
